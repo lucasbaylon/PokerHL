8a37a0273900b93cb501556e1d57180fb53eb7de
HugoErb <erb.hugo@yahoo.fr>
2022-12-30 00:05:35 +0100

    add error solution legend

--- src/app/components/training/training.component.ts
+++ src/app/components/training/training.component.ts
@@ -20,7 +20,8 @@ export class TrainingComponent implements OnInit {
 
     activeSituation!: ActiveSituation;
 
-    colorList: string[] = ["Hearts", "Diamonds", "Clubs", "Spades"]
+    colorList: string[] = ["Hearts", "Diamonds", "Clubs", "Spades"];
+    solutionColorList: string[] = ["rgb(216, 0, 0)", "rgb(0, 151, 0)", "rgb(19, 82, 255)", "rgb(140, 0, 255)", "rgb(255, 0, 212)", "rgb(255, 123, 0)", "rgb(0, 163, 228)"];
 
     constructor(
         private apiSituation: SituationService,
