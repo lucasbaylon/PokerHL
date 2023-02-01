94171f1a1795392a3df649653ac406ff75d10142
HugoErb <erb.hugo@yahoo.fr>
2023-02-01 23:15:41 +0100

    fix manage situations + for mixed situations

--- frontend/src/app/components/situations/situations.component.ts
+++ frontend/src/app/components/situations/situations.component.ts
@@ -23,6 +23,8 @@ export class SituationsComponent implements OnInit {
 
     situation_obj!: Situation;
 
+    mixedSolutionsList:any;
+
     actionSelected: "radio_action_0" | "radio_action_1" | "radio_action_2" | "radio_action_3" | "radio_action_4" | "radio_action_5" | "radio_action_6" | undefined = "radio_action_0";
 
     showOpponent2: boolean = true;
