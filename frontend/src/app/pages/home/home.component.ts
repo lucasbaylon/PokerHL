009036b36860c14f3f53590d64be52d91afc7927
HugoErb <erb.hugo@yahoo.fr>
2024-06-07 16:30:41 +0200

    suppression css home component + début refonte

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -5,8 +5,7 @@ import { Router } from '@angular/router';
     selector: 'app-home',
     standalone: true,
     imports: [],
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.scss'
+    templateUrl: './home.component.html'
 })
 export class HomeComponent {
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
-  // darkMode: 'class',
+  darkMode: 'class',
   theme: {
     screens: {
       sm: '640px',
