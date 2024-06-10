58b24223cf447f5adc1c96cd3723a2676a6f3828
Lucas Baylon <thezartop@gmail.com>
2024-06-11 00:31:09 +0200

    Fix refresh page

--- frontend/src/app/services/common.service.ts
+++ frontend/src/app/services/common.service.ts
@@ -108,4 +108,21 @@ export class CommonService {
         });
     }
 
+    /**
+     * Affiche une notification toast avec SweetAlert2.
+     * 
+     * @param {string} titre - Le titre de l'alerte
+     * @param {string} message - Le message à afficher dans la notification.
+     * @param {'success' | 'error' | 'warning' | 'info' | 'question'} [icon='success'] - L'icône à afficher dans la notification. Peut être 'success', 'error', 'warning', 'info' ou 'question'.
+     */
+    showSwalAlert(title: string, message: string, icon: 'success' | 'error' | 'warning' | 'info' | 'question' = 'success') {
+        Swal.fire({
+            title: title,
+            text: message,
+            icon: icon,
+            confirmButtonColor: "#3085d6",
+            confirmButtonText: "C'est compris !"
+        });
+    }
+
 }
