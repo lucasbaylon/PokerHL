ef03d767f8dab8b8de7801eac57dbe5dc346a8f0
Lucas Baylon <thezartop@gmail.com>
2024-06-22 18:36:15 +0200

    redirection vers page de selection de training

--- frontend/src/app/pages/select-training-mode/select-training-mode.component.ts
+++ frontend/src/app/pages/select-training-mode/select-training-mode.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Situation } from '../../interfaces/situation';
 
 @Component({
     selector: 'app-select-training-mode',
@@ -7,5 +9,25 @@ import { Component } from '@angular/core';
     templateUrl: './select-training-mode.component.html'
 })
 export class SelectTrainingModeComponent {
+
+    situationList: Situation[] = [];
+
+    constructor(
+        private router: Router,
+        private activatedRoute: ActivatedRoute
+    ) { }
+
+    ngOnInit() {
+        if (this.activatedRoute.snapshot.params.hasOwnProperty('situationList')) {
+            this.situationList = JSON.parse(this.activatedRoute.snapshot.params['situationList']);
+            const currentUrl = this.router.url;
+            const baseUrl = currentUrl.split(';')[0];
+            this.router.navigateByUrl(baseUrl);
+        }
+    }
+
+    test() {
+        console.log(this.situationList);
+    }
 
 }
