8d79d218080a93068ace2e7667d3957367cc3740
Lucas BAYLON <thezartop@gmail.com>
2024-11-13 22:23:46 +0000

    remove comment

--- database.js
+++ database.js
@@ -12,26 +12,6 @@ const pool = mysql.createPool({
     queueLimit: 0
 });
 
-// async function connectDatabase() {
-//     connection = await mysql.createConnection({
-//         host: 'localhost',
-//         user: process.env.NODE_ENV === 'production' ? "zartop" : "root",
-//         password: process.env.NODE_ENV === 'production' ? "#$W&5*grhqd^BScca6kg" : "",
-//         database: 'pokertraining',
-//         waitForConnections: true,
-//         connectionLimit: 10,
-//         queueLimit: 0
-//     });
-//     console.log('Connected to the database');
-// }
-
-// async function getConnection() {
-//     if (!connection) {
-//         await connectDatabase();
-//     }
-//     return connection;
-// }
-
 // Fonction pour obtenir une connexion du pool
 async function getConnection() {
     return await pool.getConnection();
